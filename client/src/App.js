12ce9e268638e8d05a8ddf9618720a3a0bf42b3b
Jess Schultz <schultz.jess.a@outlook.com>
2020-11-19 15:21:39 -0500

    added work space to prevent merging conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from "react"
 import './App.css';
 import {usePosition} from './Components/Location/UserLocation';
+//import for nasa
+//import for openweather
+//import for zomato
 
 
 function App() {
-
   const newLocation = usePosition();
+
   return (
     <div className="App">
-   {/* <code>
+   {
+   // <Nasa newLocation={newLocation}/>
+   // <OpenWeather/>
+   // <zomato/>
+   
+   /* <code>
       latitude: {newLocation.latitude}
       <br/>
       longitude: {newLocation.longitude}
