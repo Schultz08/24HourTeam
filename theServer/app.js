594f90eb7eda5053f6c35b5a089eab75f8cf51f9
Josh Mason <jamason87@gmail.com>
2020-11-19 19:22:17 -0500

    OpenWeather Update

--- theServer/app.js
+++ theServer/app.js
@@ -4,8 +4,14 @@ const controllers = require("./controllers");
 const app = express();
 
 app.use(require("./middleware/headers"));
+
 app.use(express.json());
 
-app.use("/nasa", controllers.nasaController);
+app.use(require("body-parser").json());
+
+//app.use("/nasa", controllers.nasaController);
 app.use("/openweather", controllers.openWeatherController);
-app.use("/zomato", controllers.zomatoController);
+//app.use("/zomato", controllers.zomatoController);
+
+//ADDED BY JOSH
+app.listen(process.env.PORT, () => console.log("Server running at http://localhost:"+process.env.PORT));
